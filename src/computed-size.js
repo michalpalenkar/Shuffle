92acb06bae76c9255d769a8f7d57161ac325a186
Michal <michalpalenkar@gmail.com>
2018-02-21 17:32:51 +0100

    document error fix

--- src/computed-size.js
+++ src/computed-size.js
@@ -1,11 +1,18 @@
-const element = document.body || document.documentElement;
-const e = document.createElement('div');
-e.style.cssText = 'width:10px;padding:2px;box-sizing:border-box;';
-element.appendChild(e);
+const computedSize = () => {
+    if ( typeof document !== 'undefined' ) {
+        const element = document.body || document.documentElement;
+        const e = document.createElement('div');
+        e.style.cssText = 'width:10px;padding:2px;box-sizing:border-box;';
+        element.appendChild(e);
+        const { width } = window.getComputedStyle(e, null);
+        element.removeChild(e);
+        return width === '10px';
+    } else {
+        return false
+    }
+}
 
-const { width } = window.getComputedStyle(e, null);
-const ret = width === '10px';
+const ret = computedSize()
 
-element.removeChild(e);
 
 export default ret;
